49ca26961d834c6d5d317632b994741d12b88a21
Julian So <ch.julian.so@gmail.com>
2024-09-11 21:22:35 -0400

    fix UI search

---
+++ src/routes/index.jsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id='zero-state'>
+      This is a list of professional soccer players from transfermarkt.
+      <br />
+      Check out <a href='https://reactrouter.com'>the docs at reactrouter.com</a>.
+    </p>
+  );
+}
